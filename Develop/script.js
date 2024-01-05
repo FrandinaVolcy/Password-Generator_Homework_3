67ec351d9d2949f4fde48c49b73c7c18ef2f4fb2
FrandinaVolcy <129709685+FrandinaVolcy@users.noreply.github.com>
2024-01-05 02:12:58 +0000

    Update

--- Develop/script.js
+++ Develop/script.js
@@ -25,21 +25,16 @@ var number = confirm ("Would you like your password to include numbers?");
 var passwordLength = prompt ("How long would you like your password? Choose between 8 to 128 characters");
 
 //Function for the length of the password
-  if (PasswordLength, 8 || PasswordLength > 128) {
+  if (passwordLength, 8 || passwordLength > 128) {
     return("Your password does not meet the criteria");
-    var passwordLength = prompt("Password must be between 8 and 128 characters.");
   }
 
-  if (uppercaseABC === false && lowercaseABC === false && specialSymbols === false && numeric === false) {
-    return "Your password does not meet the criteria";
-    var uppercaseLetter = confirm ("Would you like your password to include uppercase letters?");
-var lowercaseLetter  = confirm ("Would you like your password to include lowercase letters?");
-var specialChar = confirm ("Would you like your password to include special characters?");
-var number = confirm ("Would you like your password to include numbers?");
+  else if (uppercaseABC === false && lowercaseABC === false && specialSymbols === false && numeric === false) 
+  { return "Your password does not meet the criteria";
+  
   };
-}
 
-if (lowercaseABC) { multiSelect += lowercase; }
+if (lowercaseABC) { multiSelect += lowercaseABC; }
 if (uppercaseABC) { multiSelect += uppercaseABC; }
 if (number) { multiSelect += numbers; }
 if (specialSymbols) { multiSelect += specialSymbols; }
